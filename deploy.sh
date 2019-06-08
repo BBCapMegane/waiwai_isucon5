1e9492ae8eab02e7ad69797a655bb0b2da304ddc
Okada Shohei <okashoicircus0@gmail.com>
2019-06-08 16:51:45 +0900

    .sh 内で sudo できないから直す

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-git pull origin work
+su - isucon
 
+cd /home/isucon
+git pull origin work
 cd /home/isucon/webapp/go && PATH=/home/isucon/.local/go/bin:$PATH GOPATH=/home/isucon/webapp/go go build -o app
 
-sudo systemctl restart mysqld
-sudo systemctl restart nginx
-sudo systemctl restart isuxi.go
+logout
+
+systemctl restart mysqld
+systemctl restart nginx
+systemctl restart isuxi.go
